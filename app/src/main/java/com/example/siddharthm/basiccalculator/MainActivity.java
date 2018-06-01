9876360fab621c1666b62c6382ba816485b73d67
Siddharth M <siddharth.m98@gmail.com>
2018-06-01 22:05:46 +0530

    Little changes is UI

--- app/src/main/java/com/example/siddharthm/basiccalculator/MainActivity.java
+++ app/src/main/java/com/example/siddharthm/basiccalculator/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.siddharthm.basiccalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1;
+    Button b2;
+    Button b3;
+    Button b4;
+    Button b5;
+    Button b6;
+    Button b7;
+    Button b8;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b7 = (Button)findViewById(R.id.button5);
     }
 }
